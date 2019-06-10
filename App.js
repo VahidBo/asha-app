b89a1d174eb479e7a8d884828912ac9b5b9fe487
Vahid Boreiri <vahid.boreiri@gmail.com>
2019-06-10 15:41:58 +0430

    Add RNN for android

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to RNative!</Text>
         <Text style={styles.welcome}>this is my first line</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
