14d8d678151779ac43c2fca8f1e776cea16156d8
Vahid Boreiri <vahid.boreiri@gmail.com>
2019-07-01 22:21:11 +0430

    make a tab bar for app

--- index.js
+++ index.js
@@ -4,14 +4,45 @@
 
 import { Navigation } from 'react-native-navigation';
 import App from './App';
+import Home from './src/Components/Home';
 
-Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+const image = require('./src/Asset/Images/icon.png');
+
+Navigation.registerComponent(`HOME_PAGE`, () => Home);
+Navigation.registerComponent(`SEARCH_PAGE`, () => App);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
-      component: {
-        name: 'navigation.playground.WelcomeScreen',
+      bottomTabs: {
+        children: [{
+          stack: {
+            children: [{
+              component: {
+                name: 'HOME_PAGE',
+              },
+            }],
+            options: {
+              bottomTab: {
+                text: 'Home',
+                icon: image,
+                testID: 'FIRST_TAB_BAR_BUTTON',
+              },
+            },
+          },
+        },
+          {
+            component: {
+              name: 'SEARCH_PAGE',
+              options: {
+                bottomTab: {
+                  text: 'Search',
+                  icon: image,
+                  testID: 'SECOND_TAB_BAR_BUTTON',
+                },
+              },
+            },
+          }],
       },
     },
   });
