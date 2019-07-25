f10727d3a369a0a20322c9d633545da866eb5e10
Vahid <vahid@Vahids-MacBook-Air.local>
2019-07-25 08:22:51 -0700

    Home page is completed

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
  * @format
  */
 
-import { Navigation } from 'react-native-navigation';
+import {
+  Navigation
+} from 'react-native-navigation';
 import App from './App';
 import Home from './src/Components/Home';
 
-const image = require('./src/Asset/Images/icon.png');
+const homeIcon = require('./src/Asset/Images/home_icon.png');
+const profileIcon = require('./src/Asset/Images/account_icon.png');
+const searchIcon = require('./src/Asset/Images/search_icon.png');
 
 Navigation.registerComponent(`HOME_PAGE`, () => Home);
 Navigation.registerComponent(`SEARCH_PAGE`, () => App);
@@ -16,34 +20,73 @@ Navigation.events().registerAppLaunchedListener(() => {
     root: {
       bottomTabs: {
         children: [{
-          stack: {
-            children: [{
-              component: {
-                name: 'HOME_PAGE',
-              },
-            }],
-            options: {
-              bottomTab: {
-                text: 'Home',
-                icon: image,
-                testID: 'FIRST_TAB_BAR_BUTTON',
+            stack: {
+              children: [{
+                component: {
+                  name: 'HOME_PAGE',
+                },
+              }],
+              options: {
+                topBar: {
+                  visible: true,
+                  animate: false, // Controls whether TopBar visibility changes should be animated
+                  hideOnScroll: true,
+                  leftButtonColor: 'black',
+                  rightButtonColor: 'black',
+                  drawBehind: false,
+                  testID: 'topBar',
+                  title: {
+                    text: 'ASHA',
+                    // fontSize: 14,
+                    color: 'black',
+                    alignment: 'center',
+                    // fontFamily: 'Helvetica',
+                    // component: {
+                    //   name: 'example.CustomTopBarTitle',
+                    //   alignment: 'center'
+                    // }
+                  },
+                  background: {
+                    color: '#ffffff',
+                    // component: {
+                    //   name: 'example.CustomTopBarBackground'
+                    // }
+                  }
+                },
+                bottomTab: {
+                  text: 'Home',
+                  icon: homeIcon,
+                  testID: 'FIRST_TAB_BAR_BUTTON',
+                },
               },
             },
           },
-        },
           {
             component: {
               name: 'SEARCH_PAGE',
               options: {
                 bottomTab: {
                   text: 'Search',
-                  icon: image,
+                  icon: searchIcon,
                   testID: 'SECOND_TAB_BAR_BUTTON',
                 },
               },
             },
-          }],
+          },
+          {
+            component: {
+              name: 'SEARCH_PAGE',
+              options: {
+                bottomTab: {
+                  text: 'profile',
+                  icon: profileIcon,
+                  testID: 'SECOND_TAB_BAR_BUTTON',
+                },
+              },
+            },
+          }
+        ],
       },
     },
   });
-});
+})
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,14 +7,81 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { Card, Avatar, SocialIcon, Icon } from 'react-native-elements';
+// import Icon from 'react-native-vector-icons/MaterialIcons';
 
 type Props = {};
 export default class Home extends Component<Props> {
+  static options(passProps) {
+    return {
+      topBar: {
+        leftButtons: [
+          {
+            id: 'cameraButton',
+            icon: require('../Asset/Images/camera_icon.png')
+          }
+        ],
+        rightButtons: [
+          {
+            id: 'gameButton',
+            icon: require('../Asset/Images/game_icon.png')
+          }
+        ],
+      }
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>This is home!</Text>
+        <Card containerStyle={{}} >
+          <View style={{flexDirection: 'row'}}>
+            <Avatar
+              rounded
+              size={42}
+              source={{
+                uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+              }}
+              containerStyle={{flex: 0, marginRight: 12}}
+            />
+            <View>
+              <Text style={{fontSize: 16, fontWeight: 'bold'}}>Username</Text>
+              <Text style={{fontSize: 12}}>Location</Text>
+            </View>
+          </View>
+          <View style={{marginTop: 12}}>
+            <Text numberOfLines={2}>This is a up to 50 characters caption and up to 3 hashtags.</Text>
+          </View>
+          <View style={{marginTop: 12}}>
+            <Image
+              source={require('../Asset/Images/shooting.jpeg')}
+              resizeMode='contain'
+              style={{width: null, height: 220}}
+            />
+          </View>
+          <View style={{flexDirection: 'row'}}>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <SocialIcon
+              type='twitter'
+              light
+              raised={false}
+            />
+            <SocialIcon
+              light
+              raised={false}
+              type='instagram'
+            />
+          </View>
+          <View style={{flex: 0, flexDirection: 'row'}}>
+            <Icon
+              name='note-add'
+              type='material'
+              color='#517fa4'
+            />
+          </View>
+          </View>
+        </Card>
       </View>
     );
   }
@@ -23,8 +90,8 @@ export default class Home extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
